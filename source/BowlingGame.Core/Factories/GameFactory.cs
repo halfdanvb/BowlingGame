110c6c0c71ca52853428b962d6c23d8be3d23a53
Halfdan <hvb@firstagenda.com>
2023-05-19 14:55:33 +0200

    Added game factory and game service

---
+++ source/BowlingGame.Core/Factories/GameFactory.cs
@@ -0,0 +1,50 @@
+﻿using BowlingGame.Core.DomainModels;
+using BowlingGame.Core.Entities;
+
+namespace BowlingGame.Core.Factories;
+public static class GameFactory
+{
+    private const int FrameCount = 10;
+
+    public static GameDomain CreateGame(List<string> playerNames, int lane)
+    {
+        var rows = CreateRows(playerNames);
+
+        var gameState = new Game
+        {
+            IsOngoing = true,
+            Lane = lane,
+            Rows = rows
+        };
+
+        var gameDomain = new GameDomain(gameState);
+        return gameDomain;
+    }
+
+    private static List<Row> CreateRows(List<string> playerNames)
+    {
+        var rows = playerNames.Select((p, i) => new Row
+        {
+            PlayerName = p,
+            Order = i + 1,
+            Frames = CreateFrames()
+        }).ToList();
+
+        return rows;
+    }
+
+    private static List<Frame> CreateFrames()
+    {
+        var frames = new List<Frame>(FrameCount);
+
+        for (int i = 0; i < FrameCount; i++)
+        {
+            frames.Add(new Frame
+            {
+                Order = i + 1
+            });
+        }
+
+        return frames;
+    }
+}
---
+++ source/BowlingGame.Core/Services/GameService.cs
@@ -0,0 +1,51 @@
+﻿using BowlingGame.Core.Dto;
+using BowlingGame.Core.Factories;
+using BowlingGame.Core.Interfaces;
+
+namespace BowlingGame.Core.Services;
+public class GameService
+{
+    private readonly IGameRepository _gameRepository;
+    private readonly IGameQuery _gameQuery;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GameService(IGameRepository gameRepository, IGameQuery gameQuery, IUnitOfWork unitOfWork)
+    {
+        _gameRepository = gameRepository;
+        _gameQuery = gameQuery;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task CreateAndStart(List<string> playerNames, int lane)
+    {
+        var gameDomain = GameFactory.CreateGame(playerNames, lane);
+
+        var validationResult = gameDomain.ValidateGame();
+
+        if (validationResult.Valid == false)
+        {
+            throw new Exception(validationResult.ErrorMessage);
+        }
+
+        gameDomain.StartGame();
+
+        _gameRepository.Add(gameDomain);
+
+        await _unitOfWork.SaveChanges();
+    }
+
+    public async Task AddScore(int lane, int score)
+    {
+        var gameDomain = await _gameRepository.GetByLane(lane);
+
+        gameDomain.AddScore(score);
+
+        await _unitOfWork.SaveChanges();
+    }
+
+    public async Task<GameDto> GetScoreBoard(int lane)
+    {
+        var gameDto = await _gameQuery.GetByLaneQuery(lane);
+        return gameDto;
+    }
+}
